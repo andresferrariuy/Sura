f653dddf82b9d87a900cde6a174afcf13d723ae3
Andres Ferrari <andres.ferrari@these.com.uy>
2019-11-11 15:30:43 -0300

    Se agregan direcciones

--- spec/Insis_spec.rb
+++ spec/Insis_spec.rb
@@ -40,32 +40,63 @@ RSpec.describe 'INSIS' , js:true do
     puts fecha_nac
     puts rut
   end
-  context 'Persona' do
-    it  'física' do
-      sleep 3
-      find_by_id('pageTemplate:pt_ps1::i').trigger('click')  #Despliega el menú lateral
-      sleep 3 
-      find_by_id('pageTemplate:treeMenu:1:treeMenuLink').trigger('click')     #Click en GEN
-      sleep 3 
-      find_by_id('pageTemplate:treeMenu:14:treeMenuLink').trigger('click')     #Click en Personas
-      sleep 3
-      find_by_id('pageTemplate:treeMenu:28:treeMenuLink').trigger('click') #Click en personas
-      sleep 3
-      find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:qxmctb1').click #Click en botón para crear nueva persona
-      sleep 6
-      select('Física', :from => 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:manComp::content') #selecciono tipo de persona Física
-      fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:it22::content', with: nombre #Ingreso el nombre
-      fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:it27::content', with: apellido #Ingreso el apellido
-      fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:s1r1:0:it4::content', with: ci #Ingreso CI
-      fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:id2::content', with: fecha_nac #Ingreso Fecha de nacimiento
-      select('Hombre', :from => 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:soc2::content') #Selecciono sexo
-      select('Cédula de Identidad', :from => 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:s1r1:0:soc6::content') #selecciono tipo de documento
-      sleep 3
-      find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:globalCommit::icon').trigger('click') #Click en save
-      find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:globalClose').trigger('click') #Click en close
-      
-      
-    end
+    context 'Persona' do
+      it  'física' do
+        sleep 4
+        find_by_id('pageTemplate:pt_ps1::i').trigger('click')  #Despliega el menú lateral
+        sleep 4 
+        find_by_id('pageTemplate:treeMenu:1:treeMenuLink').trigger('click')     #Click en GEN
+        sleep 4 
+        find_by_id('pageTemplate:treeMenu:14:treeMenuLink').trigger('click')     #Click en Personas
+        sleep 4
+        find_by_id('pageTemplate:treeMenu:28:treeMenuLink').trigger('click') #Click en personas
+        sleep 4
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:qxmctb1').click #Click en botón para crear nueva persona
+        sleep 8
+        select('Física', :from => 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:manComp::content') #selecciono tipo de persona Física
+        fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:it22::content', with: nombre #Ingreso el nombre
+        fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:it27::content', with: apellido #Ingreso el apellido
+        fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:s1r1:0:it4::content', with: ci #Ingreso CI
+        fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:id2::content', with: fecha_nac #Ingreso Fecha de nacimiento
+        select('Hombre', :from => 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:soc2::content') #Selecciono sexo
+        select('Cédula de Identidad', :from => 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:s1r1:0:soc6::content') #selecciono tipo de documento
+        sleep 3
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:globalCommit::icon').trigger('click') #Click en save
+        sleep 4
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:sdi31::disAcr').trigger('click') #Click en Direcciones
+        sleep 3
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:0:pspctb1').trigger('click') #Click Crear nueva dirección
+        sleep 5
+        select('Fiscal', :from => 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:1:soc1::content') #selecciono tipo de dirección
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:1:postCodeImgLink').trigger('click') #Lupa dir postal
+        sleep 3
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:1:lovRegion:1:searchBtn').trigger('click') #Buscar dirección postal
+        find(:xpath, '//*[@id="pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:1:lovRegion:1:resultTableId::db"]/table/tbody/tr[2]').click()
+        sleep 3
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:1:lovRegion:1:resultTableId:1:tkaot3').trigger('click') #seleccionar código postal
+        sleep 3
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:1:lovRegion:1:okBtn').trigger('click') #Confirmar código postal
+        sleep 3
+        page.save_and_open_screenshot('screen1.png', full: true) # Same as save_screenshot.
+        sleep 3
+        fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:1:it21::content', with: '1234' #Ingresar número de calle
+        sleep 3
+        fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:1:it14::content', with: '1' #Ingresar Manzana
+        sleep 3
+        fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:1:it2::content', with: '1' #Ingresar Bloque
+        sleep 3
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:1:applyAddr').trigger('click') #Concatenar dir
+
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:r1:0:r5:1:submitBtn').trigger('click') #Salvar dirección
+        sleep 3
+        page.save_and_open_screenshot('screen2.png', full: true) # Same as save_screenshot.
+        sleep 3
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:globalCommit::icon').trigger('click') #Click en save
+        sleep 3
+        find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:globalClose').trigger('click') #Click en close
+
+        
+      end
 
 
       it 'jurídica' do
@@ -83,15 +114,12 @@ RSpec.describe 'INSIS' , js:true do
         select('Jurídica', :from => 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:manComp::content') #selecciono tipo de persona Jurídica
         sleep 4
         fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:it18::content', with: empresa #Ingreso razón social
-        fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:s1r1:0:it4::content', with: rut #Ingreso razón social
+        fill_in 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:s1r1:0:it4::content', with: rut #Ingreso rut
         select('R.U.T', :from => 'pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:s1r1:0:soc6::content') #selecciono tipo de documento        
-        page.save_and_open_screenshot('screen1.png', full: true) # Same as save_screenshot.
 
         find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:globalCommit::icon').trigger('click') #Click en save
-        page.save_and_open_screenshot('screen2.png', full: true) # Same as save_screenshot.
 
         find_by_id('pageTemplate:mainRegion:1:r_j_id301082:0:formTemplate1:pdynam1:1:dynam1:0:pt1:globalClose').trigger('click') #Click en close
-        page.save_and_open_screenshot('screen3.png', full: true) # Same as save_screenshot.
       end
     end
 end
